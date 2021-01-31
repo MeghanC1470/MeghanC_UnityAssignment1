071929b1ecc3fc707a94998d847bd1e9052e472e
MeghanC1470 <meghanc2000@outlook.com>
2021-01-30 16:59:15 -0800

    Unity_Exercise2

---
+++ Assets/BasicScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicScript : MonoBehaviour
+{
+
+    public Vector3 rotation = new Vector3(0, 2f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.GetComponent<Transform>().Rotate(rotation);
+    }
+}
