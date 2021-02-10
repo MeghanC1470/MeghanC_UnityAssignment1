0057eee6d2d92ed92199de8fa943cde21b47c5c0
MeghanC1470 <meghanc2000@outlook.com>
2021-02-09 23:12:00 -0800

    Exercise 3

---
+++ Assets/pickup.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public string pickuptag;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag(pickuptag))
+        {
+            Debug.Log("Collision Detected");
+            Destroy(collision.collider.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
